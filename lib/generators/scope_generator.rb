69e3cca02a5d18e0ed79ec4ef187b514373189ea
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-03-02 02:05:10 +0300

    Generates scopes and includes it to model

--- lib/generators/scope_generator.rb
+++ lib/generators/scope_generator.rb
@@ -1,13 +1,18 @@
 class ScopeGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../../scopes_rails/templates', __FILE__)
+
   def create_scope_file
     empty_directory 'app/scopes/'
     create_file "app/scopes/#{file_name}_scopes.rb", <<-FILE
 module #{class_name}Scopes
   extend ActiveSupport::Concern
+  #include StateMachineScopes
 
   included do
   end
 end
     FILE
+
+    copy_file 'scopes_rails_initializer.rb', 'config/initializers/scopes_rails_initializer.rb'
   end
 end
